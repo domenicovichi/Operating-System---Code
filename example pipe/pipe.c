84d042828c26cfb65d2f9d2645f8fbaa2a3503ec
domenicovichi <vichidomenico98@hotmail.com>
2023-05-03 18:32:48 +0200

    example of using pipe in c both unnamed and named

---
+++ example pipe/pipe.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define DIM 256
+#define READ 0
+#define WRITE 1
+
+int main()
+{
+    int n, fd[2];
+    int pid;
+    char message[DIM];
+
+    if (pipe(fd) < 0)
+    {
+        printf("pipe error");
+        exit(1);
+    }
+    if((pid=fork())<0){
+        printf("pipe error");
+        exit(1);
+    } else if (pid > 0){
+        //father
+        close(fd[READ]); //close channel not in use
+        write(fd[WRITE], "Hello son", DIM);
+    } else {
+        //son
+        close(fd[WRITE]); //close channel not in use
+        n = read(fd[READ], message, DIM);
+        printf("%s \n", message);
+    }
+
+}
